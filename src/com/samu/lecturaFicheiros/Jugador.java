e083773ab037c8fcaaf6639f8b4e3a324590ea49
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2022-03-09 12:33:39 +0100

    Minijuegos

---
+++ src/com/samu/lecturaFicheiros/Jugador.java
@@ -0,0 +1,12 @@
+package com.samu.lecturaFicheiros;
+
+import java.io.Serializable;
+
+public class Jugador implements Serializable {
+    String nombre_apellidos;
+
+    String genero;
+
+    int edad;
+
+}
--- src/com/samu/lecturaFicheiros/Main.java
+++ src/com/samu/lecturaFicheiros/Main.java
@@ -1,8 +1,30 @@
 package com.samu.lecturaFicheiros;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+// Crearemos una librería para todos estos JOptionPane
+
+        JOptionPane.showMessageDialog(null, " Bienvenido Jugador! " + "\n" + " Este juego está dividido en 3 minijuegos," +
+                " que son los siguientes :" + "\n" +
+                " 1. Piedra, Papel y Tijera " + "\n" + " 2. Adivinanza " +"\n" + " 3. Aun no se sabe ");
+
+        /*JOptionPane.showMessageDialog(null," Antes de eligir el juego con el que empezará debe introducir sus datos");
+        JOptionPane.showInputDialog(" Nombre y apellidos ");
+        JOptionPane.showInputDialog(" Género ");
+        JOptionPane.showInputDialog(" Edad ");*/
+
+        JOptionPane.showMessageDialog(null, " Ahora que sabe cual es cada juego, para iniciar el que usted desee," +
+                " lo único que debe hacer es teclear el número que corresponde con el juego que quiere jugar " + "\n"
+                );
+
+        JOptionPane.showMessageDialog(null, " ADVERTENCIA!" + "\n" + " si quiere salir del juego lo único que debe hacer es pulsar 0 ");
+
+        JOptionPane.showInputDialog(" Pulsa un número para eligir el juego que quiera ");
+
 
     }
 }
